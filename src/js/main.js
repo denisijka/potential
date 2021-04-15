dfcb7894fe158aa7eaf5a7e2d8f50a96aad3b997
denis <ovicartel@gmail.com>
2021-04-15 07:54:00 +0300

    added _skills.sass and have already done

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// const solInfoblock = document.querySelector('.solution__infoblock'),
+//     infoCard = solInfoblock.querySelector('.infoCard'),
+//     cardTitle = infoCard.querySelector('.infoCard_title');
+
+// cardTitle.forEach(function (item) {
+//     item.remove();
+// });
+
