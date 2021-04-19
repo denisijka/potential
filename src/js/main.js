22760720b196655969dcfef0c625b5a6c819426b
denis <ovicartel@gmail.com>
2021-04-19 09:18:59 +0300

    added footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,19 @@
-'use strict';
-const link = document.querySelector('a');
+// 'use strict';
+// const link = document.querySelector('.links'),
+//     realize = document.querySelector('.realize__title'),
+//     helpers = document.querySelector('.helpers__item');
 
-link.addEventListener('click', (event) => {
-    event.defaultPrevented();
-});
+
+
+// const deleteElement = (e) => {
+//     console.log(e.target);
+//     console.log(e.type);
+// };
+
+// link.addEventListener('click', deleteElement);
+// helpers.addEventListener('click', deleteElement);
+
+// link.textContent = 'hello mutherfucker';
+// link.style.color = 'red';
+
+
