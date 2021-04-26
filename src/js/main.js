1e70d37d0fcda9c547f9b0db1a04eaa55a6a67cc
denis <ovicartel@gmail.com>
2021-04-26 21:17:35 +0300

    @media (max-width: 767px) has started .gamburger

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
-// 'use strict';
-// window.addEventListener('DOMContentLoaded', () => {
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.promo__menu'),
+        menuItem = document.querySelectorAll('.promo__menu-item'),
+        hamburger = document.querySelector('.hamburger');
 
-//     const audDescr = document.querySelector('.audience__descr');
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
 
-
-
-// });
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    });
+});
 
