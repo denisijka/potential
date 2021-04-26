b701a209ee55e2856ccf5246334b9836340d8a5c
denis <ovicartel@gmail.com>
2021-04-26 07:37:25 +0300

    @media 991px  - playVideo has done

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,9 @@
 // 'use strict';
-// const link = document.querySelector('.links'),
-//     realize = document.querySelector('.realize__title'),
-//     helpers = document.querySelector('.helpers__item');
+// window.addEventListener('DOMContentLoaded', () => {
+
+//     const audDescr = document.querySelector('.audience__descr');
 
 
 
-// const deleteElement = (e) => {
-//     console.log(e.target);
-//     console.log(e.type);
-// };
-
-// link.addEventListener('click', deleteElement);
-// helpers.addEventListener('click', deleteElement);
-
-// link.textContent = 'hello mutherfucker';
-// link.style.color = 'red';
-
+// });
 
